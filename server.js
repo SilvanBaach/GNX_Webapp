ac7213584aaab19fa6d4ad0f85f96353ff7f538e
baachsil <baachsil@students.zhaw.ch>
2023-03-10 11:00:57 +0100

    implemented postgres connection

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const {pool} = require('./backend/database/dbConfig');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/frontend/index.html');
 });
 
 app.listen(3000, () => {
