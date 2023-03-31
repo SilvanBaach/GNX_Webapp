cd28135c2e1c575a58e048714ee4f07e210711cd
Jon Defilla <jon.defilla@swisscom.com>
2023-03-31 10:25:42 +0200

    git commit -m "feat(doc): fix vuepress pipeline [NOTICKET]"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,30 @@
+import { defaultTheme } from '@vuepress/theme-default'
 module.exports = {
     base: "/GNX_Webapp/",
+    lang: 'en-US',
     title: "GNX Webapp",
+    description: "The one and only GNX Webapp", //theme: '@vuepress/theme-default', // don't enable, breaks. Maybe need to install it first?
+    theme: defaultTheme({
+    //logo: "/images/logo.png",
+    editLink: false,
+    smoothScroll: true,
+    darkMode: true,
+    sidebar: false,
+    lastUpdated: true,
+    contributors: false,
+    search: false,
+    themePlugins: { // * disable or enable plugins
+      externalLinkIcon: true, // ! do not disable because it breaks deployment on netlify
+      backToTop: false,
+    },
+    navbar: [
+      {
+        text: "Privacy Notice",
+        link: "/data-collection-and-privacy-notice/",
+      },
+    ],
+  }),
+
+
+    plugins: [],
 }
