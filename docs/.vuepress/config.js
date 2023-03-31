40d68ab7beee9f8bc3e02a50e17de729c49c06eb
Jon Defilla <jon.defilla@swisscom.com>
2023-03-31 10:00:49 +0200

    git commit -m "feat(doc): fix vuepress pipeline [NOTICKET]"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    // other config options...
     base: "/GNX_Webapp/",
-    plugins: [
-        [
-            'vuepress-plugin-cat',
-            {
-                inputFile: 'index.md', // the name of the file that will be created
-                inputDir: './docs', // the directory where the files to be concatenated are located
-                outputDir: './docs', // the directory where the generated HTML file will be located
-                separator: '\n\n' // the separator to use between the concatenated files
-            }
-        ]
-    ]
+    title: "GNX Webapp",
 }
