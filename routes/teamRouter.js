2cf06bbc69ad084fc74bfba2ee1d8d0a4799cab7
badrli <Windows199699!>
2023-04-23 21:41:16 +0200

    feat(Team-management): Created routes for inserting teams and teamTypes [#56]

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const router = express.Router();
 const {pool} = require('/js/serverJS/database/dbConfig.js');
+const util = require("util");
 
 /**
  * GET route for getting all teams
@@ -17,11 +18,43 @@ router.post('/getteams', function (req, res) {
 });
 
 /**
+ * POST route for inserting a new team
+ */
+router.post('/insertteam', function (req, res) {
+    const formData = req.body;
+
+    insertTeam(formData).then((result) => {
+        if (result === -1) {
+            res.status(500).send({message: "There was an error inserting the team! Please try again later."});
+        }else {
+            res.status(200).send({message: "Team inserted successfully"});
+        }
+    }).catch(() => {
+        res.status(500).send({message: "There was an error inserting the team! Please try again later."});
+    });
+});
+
+/**
  * Get all teams
  * @returns {Promise<*>} a Promise that resolves to an array of teams
  */
 function getTeams() {
     return pool.query(`SELECT * FROM team ORDER BY displayname`);
+}
+
+/**
+ * Insert a new team
+ * @param data the data of the team
+ * @returns {Promise<number>} a Promise that resolves to the id of the new team
+ */
+function insertTeam(data) {
+    results = pool.query(`INSERT INTO team (displayname, teamtype_fk, weight) VALUES ($1,$2,$3) RETURNING id`,[data.teamName, data.teamType, data.teamWeight]);
+
+    if (results.rows.length === 0) {
+        return -1;
+    }else {
+        return 0;
+    }
 }
 
 module.exports = router;
--- routes/teamTypeRouter.js
+++ routes/teamTypeRouter.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const router = express.Router();
 const {pool} = require('/js/serverJS/database/dbConfig.js');
-const util = require("util");
+
 /**
  * GET route for getting all team types
  */
@@ -17,11 +17,44 @@ router.post('/getteamtypes', function (req, res) {
 });
 
 /**
+ * POST route for inserting a new team type
+ */
+router.post('/insertteamtype', function (req, res) {
+    const formData = req.body;
+
+    insertTeamType(formData).then((result) => {
+        if (result === -1) {
+            res.status(500).send({message: "There was an error inserting the team type! Please try again later."});
+        }else {
+            res.status(200).send({message: "Team type inserted successfully"});
+        }
+    }).catch(() => {
+        res.status(500).send({message: "There was an error inserting the team type! Please try again later."});
+    });
+});
+
+
+/**
  * Get all team types
  * @returns {Promise<*>} a Promise that resolves to an array of team types
  */
 function getTeamTypes() {
     return pool.query(`SELECT * FROM teamtype ORDER BY displayname`);
+}
+
+/**
+ * Insert a new team type
+ * @param data the data of the team type
+ * @returns {Promise<number>} a Promise that resolves to the id of the new team type
+ */
+async function insertTeamType(data) {
+    results = pool.query(`INSERT INTO teamtype (name, displayname) VALUES ($1,$2) RETURNING id`,[data.internalName, data.displayName]);
+
+    if (results.rowCount === 0) {
+        return -1;
+    }else {
+        return 0;
+    }
 }
 
 module.exports = router;
