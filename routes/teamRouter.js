2cf240893b7b7900184f74f7c7d70262af19f381
badrli <Windows199699!>
2023-04-24 15:51:30 +0200

    feat(Team-management): Created more routes for team manipulation [#56]

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -35,6 +35,40 @@ router.post('/insertteam', function (req, res) {
 });
 
 /**
+ * POST route for deleting a team
+ */
+router.post('/deleteteam', function (req, res) {
+    const formData = req.body;
+
+    deleteTeam(formData).then((result) => {
+        if (result === -1) {
+            res.status(500).send({message: "There was an error deleting the team! Please try again later."});
+        }else {
+            res.status(200).send({message: "Team deleted successfully"});
+        }
+    }).catch(() => {
+        res.status(500).send({message: "There was an error deleting the team! Please try again later."});
+    });
+});
+
+/**
+ * POST route for updating a team
+ */
+router.post('/updateteam', function (req, res) {
+    const formData = req.body;
+
+    updateTeam(formData).then((result) => {
+        if (result === -1) {
+            res.status(500).send({message: "There was an error updating the team! Please try again later."});
+        }else {
+            res.status(200).send({message: "Team updated successfully"});
+        }
+    }).catch(() => {
+        res.status(500).send({message: "There was an error updating the team! Please try again later."});
+    });
+});
+
+/**
  * Get all teams
  * @returns {Promise<*>} a Promise that resolves to an array of teams
  */
@@ -56,5 +90,36 @@ function insertTeam(data) {
         return 0;
     }
 }
+
+/**
+ * Delete a team
+ * @param id the id of the team to delete
+ * @returns {Promise<number>} a Promise that resolves to the id of the new team
+ */
+function deleteTeam(id) {
+    results = pool.query(`DELETE FROM team WHERE id = $1`,[id]);
+
+    if (results.rowCount === 0) {
+        return -1;
+    }else {
+        return 0;
+    }
+}
+
+/**
+ * Update a team
+ * @param data the data of the team
+ * @returns {number} the id of the updated team
+ */
+function updateTeam(data) {
+    results = pool.query(`UPDATE team SET displayname = $1, teamtype_fk = $2, weight = $3 WHERE id = $4`,[data.teamName, data.teamType, data.teamWeight, data.id]);
+
+    if (results.rowCount === 0) {
+        return -1;
+    }else {
+        return 0;
+    }
+}
+
 
 module.exports = router;
