1155986486bc24e242960bba01e9b06cbcc24358
badrli <outitbad@students.zhaw.ch>
2023-05-19 13:09:58 +0200

    feat(Dashboard): Created first draft of the home-page[#61]

---
+++ js/clientJS/home.js
@@ -0,0 +1,33 @@
+function buildNextTrainingTable1(teamId){
+    const url = "/presence/nextTrainings/" + teamId;
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            const tableBody = $("#training-column");
+            tableBody.empty();
+
+            data.forEach(function(training) {
+                const tr = $("<tr></tr>");
+                const tdDate = $("<td></td>").text(training.readable_date);
+                const tdFrom = $("<td></td>").text(training.starttime);
+                const tdUntil = $("<td></td>").text(training.endtime);
+                const tdType = $("<td></td>");
+
+                const statusIndicator = $("<div></div>").addClass("status-indicator");
+                if (training.trainingtype === "sure") {
+                    statusIndicator.addClass("status-green");
+                } else {
+                    statusIndicator.addClass("status-orange");
+                }
+                tdType.append(statusIndicator)
+
+                tr.append(tdDate).append(tdFrom).append(tdUntil).append(tdType);
+                tableBody.append(tr);
+            });
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+}
