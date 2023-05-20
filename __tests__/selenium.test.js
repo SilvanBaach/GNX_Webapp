e9b08e5024461c66af06b9a65cc705661d7d8121
Jon Defilla <jon.defilla@swisscom.com>
2023-05-20 14:20:36 +0200

    selenium test working pidar blyat

---
+++ __tests__/selenium.test.js
@@ -0,0 +1,38 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const app = require('../server');
+
+const port = process.env.PORT || 3000;
+const address = process.env.ADDRESS || '0.0.0.0'; // Add this line
+
+describe('Example Selenium Test', () => {
+  let driver;
+
+  beforeAll(async () => {
+    // Start the server
+    app.listen(port, address, (err) => {
+      console.log(`Genetix Webapp listening at http://${address}:${port}`);
+    });
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+    // Close the server after all tests are done
+    app.close((err) => {
+      if (err) {
+        console.error('Error closing server:', err);
+      } else {
+        console.log('Server closed successfully');
+      }
+    });
+  });
+
+  test('Test Connection', async () => {
+    await driver.get('http://0.0.0.0:3000');
+    //const searchBox = await driver.findElement(By.name('q'));
+    //await searchBox.sendKeys('OpenAI', Key.RETURN);
+    await driver.wait(until.titleContains('GNX Webapp'), 5000);
+    const title = await driver.getTitle();
+    expect(title).toContain('GNX Webapp');
+  });
+});
