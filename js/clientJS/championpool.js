d2c274cb719e25e4cb1fd7e3bed28171f54b3c0c
badro <outitbad@students.zhaw.ch>
2023-07-20 10:48:09 +0200

    feat(Championpool):  Added custom Attributes and set them when adding a champion [#109]

--- js/clientJS/championpool.js
+++ js/clientJS/championpool.js
@@ -1,46 +1,83 @@
+const rowCount = 6;
+const columnCount = 7;
 let currentTarget = null;
-function buildChampionpoolHeader(headerElement, playerArray) {
-    var header = document.querySelector(headerElement);
-    var playerNames = ['', 'Top', 'Jungle', 'Mid', 'ADC', 'Support'];
 
-    //TODO: Use the playerArray to build the header
+/**
+ * Build the header for each championpool table
+ * @param headerElement The element where the header should be placed inside
+ */
+function buildChampionpoolHeader(headerElement) {
+    var header = document.querySelector(headerElement);
+    var headerTerms = ['', 'Top', 'Jungle', 'Mid', 'ADC', 'Support'];
 
     // Create the header
-    header.innerHTML = playerNames
-        .map(function (day) {
+    header.innerHTML = headerTerms
+        .map(function (headerTerm) {
             var headerClass = "grid-header";
             return (
                 '<div class="grid-item ' +headerClass + '">' +
                 '<div class="header-content-container">' +
-                '<p>' + day + '</p>' +
+                '<p>' + headerTerm + '</p>' +
                 '</div>' +
                 '</div>'
             );
         })
         .join("");
 }
-function buildChampionpoolContainer(containerElement, playerArray) {
+
+/**
+ * Build the container for each championpool table
+ * @param containerElement The element where the header should be placed inside
+ * @param tableData The tableData should be a array which includes the position of and the text for each cell
+ */
+function buildChampionpoolContainer(containerElement, tableData) {
+    const tableType = containerElement.split("-")[0].replace(".", "");
     var container = document.querySelector(containerElement);
-    var playerNames = ['None', 'Top', 'Jungle', 'Mid', 'ADC', 'Support'];
-    var indexCounter = 1; // Initialize the index counter
-    for (var i = 0; i < 7; i++) {
+    fillArrayWithNull(tableData);
+    for (var i = 0; i < columnCount; i++) {
+        let counter = 0;
         var headerClass = "row-container";
-        var htmlContent = playerNames
-            .map(function (player, index) {
-                if (player === 'None') {
-                    var currentIndex = indexCounter++; // Increment the index counter
-                    return '' +
-                        '<div class="grid-item ' + headerClass + ' grid-item-main">' +
-                        '<div class="champion-container">' +
-                        '<p class="champion-main">Main ' + currentIndex + '</p>' +
-                        '</div></div>';
+        var htmlContent = tableData
+            .map(function (dataArray) {
+                counter++; // Increment the counter with each iteration of the map function
+                if (counter % columnCount === 1) {
+                    if (dataArray === null) {
+                        return '' +
+                            '<div class="grid-item ' + headerClass + ' grid-item-main" ' +
+                            addAttributesToElements(tableType, i, counter-1, "Main " + i, null) +
+                            '>' +
+                            '<div class="champion-container">' +
+                            '<p class="champion-main">' + "Main" + " " + i + '</p>' +
+                            '</div></div>';
+                    } else {
+                        return '' +
+                            '<div class="grid-item ' + headerClass + ' grid-item-main" ' +
+                            addAttributesToElements(tableType, i, counter-1, dataArray.championName, null) +
+                            '>' +
+                            '<div class="champion-container">' +
+                            '<p class="champion-main">' + dataArray.championName + '</p>' +
+                            '</div></div>';
+                    }
                 } else {
-                    return '' +
-                        '<div class="grid-item ' + headerClass + '">' +
-                        '<div class="champion-container">' +
-                        '<span class="championname-span">Aatrox</span>' +
-                        '<img class="champion-img" src="https://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/Aatrox.png" alt="Champion Image">' +
-                        '</div></div>';
+                    if (dataArray === null) {
+                        return '' +
+                            '<div class="grid-item ' + headerClass + '" ' +
+                            addAttributesToElements(tableType, i, counter-1, "PlaceHolder", counter-1) +
+                            '>' +
+                            '<div class="champion-container">' +
+                            '<span class="championname-span">PlaceHolder</span>' +
+                            '<img class="champion-img" src="../../res/others/blank_profile_picture.png" alt="Champion Image">' +
+                            '</div></div>';
+                    } else {
+                        return '' +
+                            '<div class="grid-item ' + headerClass + '" ' +
+                            addAttributesToElements(tableType, i, counter-1, dataArray.championName, counter-1) +
+                            '>' +
+                            '<div class="champion-container">' +
+                            '<span class="championname-span">' + dataArray.championName + '</span>' +
+                            '<img class="champion-img" src="https://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/' + dataArray.championName + '.png" alt="Champion Image">' +
+                            '</div></div>';
+                    }
                 }
             })
             .join("");
@@ -48,27 +85,43 @@ function buildChampionpoolContainer(containerElement, playerArray) {
         container.innerHTML += htmlContent;
     }
 }
+
+
+
+/**
+ * This method setups the championpool Page
+ * This includes the header, the container and the popup
+ * @returns {Promise<void>}
+ */
 async function setupChampionpool() {
-    const headerElement = ".main-chp-header";
+    const headerElement = [".main-chp-header", ".practice-chp-header", ".suggestions-chp-header"];
     const playerArray = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5'];
 
     const popupChampions = new Popup("popup-containerChampions");
     popupChampions.displayPopupWithTable(getChampionNameAndPictureFromDDragon(await getDDragonData())
     )
 
-    buildChampionpoolHeader(headerElement, playerArray);
-    buildChampionpoolContainer(headerElement, playerArray);
+    headerElement.forEach(buildChampionpoolHeader);
+    buildChampionpoolContainer(headerElement[0], await getChampionpoolData());
     $(".champion-container").click(function (e) {
+        const $target = $(e.currentTarget);
+        const $gridItem = $target.closest(".grid-item"); // Find the closest ancestor with class 'grid-item'
+
         popupChampions.open(e);
-        currentTarget = e.currentTarget
+        currentTarget = $gridItem; // Set currentTarget to the .grid-item element
     });
+
 
     $(".champion-container-popup").click(function (e) {
         const $target = $(e.currentTarget);
-        const $span = $target.find('span').clone();
-        const $img = $target.find('img').clone();
+        const championName = $target.find('span').text(); // Get the inner text of the .champion-container-popup
+        const championSrc = $target.find('img').attr("src"); // Get the src attribute of the image in .champion-container-popup
 
-        $(currentTarget).html($span).append($img);
+        $(currentTarget).attr("data-championname", championName); // Set the data-championname attribute of currentTarget with championName
+        $(currentTarget).find('span').text(championName); // Replace the inner text of currentTarget with championName
+        $(currentTarget).find('img').attr("src", championSrc); // Replace the src attribute of the image in currentTarget with championSrc
+
+        updateChampionpoolData();
     });
 
 }
@@ -103,4 +156,83 @@ function getChampionNameAndPictureFromDDragon(dDragon){
         championNameAndPicture.push([championName, pictureUrl])
     }
     return championNameAndPicture;
+}
+
+/**
+ * Gets all users from the database from one team
+ * @returns {Promise<void>}
+ */
+async function getChampionpoolData() {
+    let championpoolData = null;
+    await $.ajax({
+        url: "/riot/getChampionpool/",
+        type: "GET",
+        success: function (data) {
+            championpoolData = data;
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+
+    return championpoolData;
+}
+
+/**
+ * Updates the data of a Team
+ */
+function updateChampionpoolData() {
+    const championpoolData = [];
+
+    $(".row-container").each(function() {
+        const playerOfChampion = null;
+        const editorOfChampion = null
+        const championpoolTableType = null
+        const lane= $(this).id % columnCount;
+        const row = Math.floor($(this).id / columnCount)
+        const championName = $(this).innerText;
+        //TODO: If there are more than 1 team check of teamNames
+        const team = 0;
+        championpoolData.push([playerOfChampion, editorOfChampion, championpoolTableType, lane, row, championName, team]);
+    });
+
+    $.ajax({
+        url: "/riot/updateChampionpool",
+        type: "POST",
+        data: {
+            championpoolData: championpoolData
+        },
+        success: function() {
+            console.log("Successfully updated championpool data!");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Error updating Championpool", errorThrown);
+        }
+    });
+}
+
+/**
+ * This method is used to fill an array with null values until it has the length of the rowCount
+ * @param array The array which should be filled with null values
+ * @returns {*} The array with the null values
+ */
+function fillArrayWithNull(array) {
+    const minLength = rowCount;
+    const currentLength = array.length;
+
+    if (currentLength < minLength) {
+        for (let i = currentLength; i < minLength; i++) {
+            array.push(null);
+        }
+    }
+
+    return array;
+}
+function addAttributesToElements(tableType, column, row, championName, playerOfChampion) {
+    let customAttributes = 'data-tableType="' + tableType + '" ' +
+        'data-column="' + column + '" ' +
+        'data-row="' + row + '" ' +
+        'data-championName="' + championName + '" ' +
+        'data-playerOfChampion="' + playerOfChampion + '"';
+    return customAttributes;
 }
--- routes/riotRouter.js
+++ routes/riotRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const riot = require('../js/serverJS/riot.js')
+const {checkNotAuthenticated} = require("../js/serverJS/sessionChecker");
+const {pool} = require("../js/serverJS/database/dbConfig");
 
 
 /**
@@ -11,4 +13,53 @@ router.get('/getDDragonData', async (req, res) => {
     res.send(championData);
 });
 
+/**
+ * GET route for getting the championpool data
+ */
+router.get('/getChampionpool',  checkNotAuthenticated, function (req, res) {
+    getChampionpool().then((result) => {
+        res.status(200).send(result.rows);
+    }).catch(() => {
+        res.status(500).send({message: "There was an error getting the championpool data."});
+    });
+});
+
+/**
+ * POST route for updating a team
+ */
+router.post('/updateChampionpool', checkNotAuthenticated, function (req, res) {
+    const formData = req.body;
+
+    updateOrInsertChampionpool(formData).then((result) => {
+        if (result.rowCount === 0) {
+            res.status(500).send({message: "There was an error updating the championpool! Please try again later."});
+        }else {
+            res.status(200).send({message: "Championpool updated successfully"});
+        }
+    }).catch(() => {
+        res.status(500).send({message: "There was an error updating the championpool! Please try again later."});
+    });
+});
+
+function getChampionpool() {
+    return pool.query(`SELECT * FROM championpool ORDER BY id`);
+}
+
+function updateOrInsertChampionpool(data) {
+    let promises = [];
+    //Checke ob ein Eintrag existiert mit folgender Bedinung (Gibt bereits type, lane, subtype)
+    for(let i = 0; i < data.length; i++){
+        if(checkIfChampionpoolEntryExists(data).rows[0] == false){
+            pool.query(`INSERT INTO championpool (type, lane, subtype, champion) VALUES ($1, $2, $3, $4)`, [data[i].type, data[i].lane, data[i].subtype, data[i].champion]);
+        }
+        else{
+            pool.query(`UPDATE championpool SET champion = $1 WHERE id = $2`,[arrayOfTextAndID[0], arrayOfTextAndID[1]]);
+        }
+    }
+    return Promise.all(promises);
+}
+
+function checkIfChampionpoolEntryExists(data) {
+    return pool.query(`SELECT * FROM championpool WHERE type = $1 AND lane = $2 AND subtype = $3`, [data.type, data.lane, data.subtype]);
+}
 module.exports = router;
