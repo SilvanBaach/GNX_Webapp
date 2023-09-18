46edd203bf43519ce3f2764bf7eb7066afcd052e
Silvan Baach <silvan.baach@icloud.com>
2023-09-18 17:06:01 +0200

    .

--- js/clientJS/trainingnotes.js
+++ js/clientJS/trainingnotes.js
@@ -1,5 +1,9 @@
 let currentNote = null;
+let allNotes = [];
 let currentNoteSections = [];
+let currentSection = null;
+const popupDelSection = new Popup("popupContainerDelSection");
+let sectionIndexToBeDeleted = null;
 
 /**
  * This function holds all logic for the initialization of the training notes page
@@ -7,24 +11,58 @@ let currentNoteSections = [];
 function initTrainingNotes() {
     $("#noteDisplayContainer").hide();
     $("#editNoteContainer").hide();
-    $("#saveNote").hide().click(function() {
+    $("#editBtnContainer").hide();
+    $("#saveNote").off('click').click(function() {
         saveNote();
     });
-    $("#editNote").hide().click(function() {
+    $("#editNote").hide().off('click').click(function() {
         editNote();
+    });
+    $("#discardChanges").off('click').click(function() {
+        displayTrainingNote(currentNote.id)
+    });
+    $("#newNote").off('click').click(function() {
+        createNewNote();
     });
 
     setupAddSectionPopup();
+    setupDelSectionPopup();
+    setupDelNotePopup();
+
+    $(document).off('click', '.edit-icon').on('click', '.edit-icon', function(e) {
+        e.preventDefault();
+
+        const sectionElement = $(this).closest('.section');
+        const sectionIndex = $("#sectionContainer .section").index(sectionElement);
+
+        $.each(currentNoteSections, function(index, section) {
+            let element = $(section.fieldRef);
+            section.value = element.val();
+        });
+
+        if ($(this).children().hasClass('ri-arrow-up-line')) {
+            moveSectionUp(sectionIndex);
+            editNote();
+        } else if ($(this).children().hasClass('ri-arrow-down-line')) {
+            moveSectionDown(sectionIndex);
+            editNote();
+        } else if ($(this).children().hasClass('ri-close-circle-line')) {
+            sectionIndexToBeDeleted = sectionIndex;
+            popupDelSection.open(e);
+        }
+    });
 }
 
 /**
  * Builds the existing notes table
  */
-function buildExistingNodesTable(){
+function buildExistingNodesTable() {
     let table = $('#existingNotesData');
     table.empty();
 
     loadExistingNotes().then((result) => {
+        allNotes = result;
+
         for (let i = 0; i < result.length; i++) {
             let note = result[i];
             // Create a new row and add it to the table
@@ -35,9 +73,16 @@ function buildExistingNodesTable(){
             newRow.append($('<td>').text(note.editor));
             newRow.append($('<td>').text(note.lastedited));
             let viewButton = $('<button class="default purple" style="height: 30px"><i class="ri-eye-line ri-lg"></i>View</button>');
+            viewButton.attr('data-id', note.id);
             viewButton.on('click', function() {
-                displayTrainingNote(note);
+                let id = $(this).attr('data-id');
+                if ($("#editNoteContainer").is(":visible")) {
+                    displayError("Please save or cancel your changes first!")
+                }else {
+                    displayTrainingNote(id);
+                }
             });
+
             newRow.append($('<td>').append(viewButton));
             table.append(newRow);
         }
@@ -62,17 +107,63 @@ function loadExistingNotes(){
 
 /**
  * Displays the training note with the given noteId
- * @param note
+ * @param noteId
  */
-function displayTrainingNote(note) {
-    $("#nothingPlaceholder").hide();
-    $("#noteDisplayContainer").show();
-    $("#editNote").show();
+function displayTrainingNote(noteId) {
+    if (!noteId) {
+        $("#editBtnContainer").hide();
+        $("#editNoteContainer").hide();
+        $("#editNote").hide();
+        $("#nothingPlaceholder").show();
+        return;
+    }
 
-    $("#noteTitle").text(note.title);
-    $("#noteCreation").html("Created at <strong>" + note.created + "</strong> by <strong>" + note.creator + "</strong>");
+    currentNote = allNotes.find(note => note.id == noteId);
 
-    currentNote = note;
+    if (!currentNote) {
+        console.error("Note with ID " + noteId + " not found in allNotes.");
+        return;
+    }
+
+    //Load the sections
+    getSections(noteId).then((result) => {
+        currentNoteSections = result;
+
+        $("#nothingPlaceholder").hide();
+        $("#editBtnContainer").hide();
+        $("#editNoteContainer").hide();
+        $("#noteDisplayContainer").show();
+        $("#sectionDisplayContainer").empty();
+        $("#editNote").show();
+
+        $("#noteTitle").text(currentNote.title);
+        $("#noteCreation").html("Created at <strong>" + currentNote.created + "</strong> by <strong>" + currentNote.creator + "</strong>");
+
+        //Add all the Sections to display them
+        currentNoteSections.forEach(function (section) {
+            currentSection = section;
+            addSection(section.type,0, section.value);
+        });
+    });
+}
+
+/**
+ * This function returns all sections for a training note
+ * @param noteId
+ * @returns {JQuery.jqXHR}
+ */
+function getSections(noteId){
+    return $.ajax({
+        url: '/trainingNotes/getSections',
+        type: 'GET',
+        data: {noteId: noteId},
+        error: function(data) {
+            if (data.responseJSON && data.responseJSON.redirect) {
+                window.location.href = data.responseJSON.redirect;
+            }
+            console.log("Error fetching sections:", data.responseJSON);
+        }
+    })
 }
 
 /**
@@ -81,21 +172,45 @@ function displayTrainingNote(note) {
 function editNote() {
     $("#noteDisplayContainer").hide();
     $("#editNote").hide();
-    $("#saveNote").show();
+    $("#editBtnContainer").show();
     $("#editNoteContainer").show();
+    $("#sectionContainer").empty();
 
     $("#noteCreationEdit").html("Created at <strong>" + currentNote.created + "</strong> by <strong>" + currentNote.creator + "</strong>");
     $("#editTitle").val(currentNote.title);
+
+    //Add all the Sections to display them
+    for(let i = 0; i < currentNoteSections.length; i++) {
+        addSection(currentNoteSections[i].type, 1, currentNoteSections[i].value, false, i+1);
+    }
 }
 
 /**
  * Function which saves the note
  */
 function saveNote(){
-    $("#noteDisplayContainer").show();
-    $("#editNote").show();
-    $("#saveNote").hide();
-    $("#editNoteContainer").hide();
+    $.each(currentNoteSections, function(index, section) {
+        let element = $(section.fieldRef);
+        section.value = element.val();
+    });
+
+    for (let i = 0; i < currentNoteSections.length; i++) {
+        currentNoteSections[i].order = i+1;
+    }
+
+    currentNote.title = $("#editTitle").val();
+
+    upsertTrainingNote().then(() => {
+        $("#noteDisplayContainer").show();
+        $("#editNote").show();
+        $("#editBtnContainer").hide();
+        $("#editNoteContainer").hide();
+        $("#sectionDisplayContainer").empty();
+
+        buildExistingNodesTable();
+        currentNote = allNotes[0];
+        displayTrainingNote(currentNote.id);
+    });
 }
 
 /**
@@ -114,70 +229,150 @@ function setupAddSectionPopup(){
 
     $("#btnAddSection").click(function () {
         popupAddSection.close()
-        addSection($("#sectionDropdown").val());
+        addSection($("#sectionDropdown").val(),1,"",true, currentNoteSections.length+1);
+    });
+}
+
+/**
+ * Function which opens the popup for deleting a new section
+ */
+function setupDelSectionPopup(){
+    popupDelSection.displayYesNoPopup('/res/others/alert.png','Delete Section','Do you really want to delete this section?','No','Yes','btnNoDelSection', 'btnYesDelSection')
+
+    $("#btnNoDelSection").click(function () {
+        popupDelSection.close()
+    });
+
+    $("#btnYesDelSection").click(function () {
+        deleteSection(sectionIndexToBeDeleted)
+        sectionIndexToBeDeleted = null;
+        popupDelSection.close()
+    });
+}
+
+/**
+ * Function which setups the popup for deleting a note
+ */
+function setupDelNotePopup(){
+    const popupDelNote = new Popup("popupContainerDelSection");
+    popupDelNote.displayYesNoPopup('/res/others/alert.png','Delete Note','Do you really want to delete this note?','No','Yes','btnNoDelNote', 'btnYesDelNote')
+
+    $("#deleteNote").click(function (e) {
+        popupDelNote.open(e);
+    });
+
+    $("#btnNoDelNote").click(function () {
+        popupDelNote.close()
+    });
+
+    $("#btnYesDelNote").click(function () {
+        deleteNote();
+        popupDelNote.close()
     });
 }
 
 /**
  * This function adds a new section to the note
  * @param type
+ * @param mode 0 = display, 1 = edit
+ * @param value the value of the field
+ * @param newSection true if the section is new
+ * @param internalSectionId the id of the section in the array
  */
-function addSection(type){
+function addSection(type, mode, value, newSection = false, internalSectionId) {
     let html;
 
+    if (typeof type === 'string') {
+        type = parseInt(type);
+    }
+
     switch (type) {
-        case '1':
-            html = addTitleSection();
+        case 1:
+            html = addTitleSection(mode, value, newSection, internalSectionId);
             break;
-        case '2':
-            html = addSimpleTextSection();
+        case 2:
+            html = addSimpleTextSection(mode, value, newSection, internalSectionId);
             break;
-        case '3':
-            html = addRichTextSection();
+        case 3:
+            html = addRichTextSection(mode, value, newSection, internalSectionId);
             break;
-        case '4':
+        case 4:
             break;
-        case '5':
+        case 5:
             html = addVideoSection();
             break;
-        case '6':
+        case 6:
             break;
     }
 
-    $("#sectionContainer").append(html);
-    if(type === '3'){
-        currentNoteSections[currentNoteSections.length-1].editor = new RichTextEditor("#editor" + currentNoteSections.length);
+    if (mode === 0){
+        $("#sectionDisplayContainer").append(html);
+    }else {
+        $("#sectionContainer").append(html);
+        if (type === 3 && newSection) {
+            currentNoteSections[currentNoteSections.length - 1].editor = new RichTextEditor("#editor" + internalSectionId);
+        }
     }
 }
 
 /**
  * This function returns the html for a new title section
  */
-function addTitleSection(){
-    let html= `<div class="section">
+function addTitleSection(mode, value, newSection, internalSectionId){
+    let html;
+
+    if(mode === 0){
+        html = `<div class="section">
+                    <p class="section-display-title">${value}</p>
+                </div>`
+    }else {
+        if (newSection) {
+            currentNoteSections.push({type: 1, editor: null, fieldRef: "#newTitle" + internalSectionId, order: internalSectionId});
+        }else{
+            currentNoteSections[internalSectionId-1].fieldRef = "#newTitle" + internalSectionId;
+        }
+
+        html = `<div class="section">
                         <div class="new-title-container">
+                            ${getActionHtml()}
                             <label class="label">Section Title</label>
-                            <input type="text" class="input" id="newTitle${currentNoteSections.length+1}" style="width: 500px"/>
+                            <input type="text" class="input" id="newTitle${internalSectionId}" style="width: 500px" value="${value}"/>
                         </div>
                       </div>`;
-
-    currentNoteSections.push({type: 1, editor: null, fieldRef: "#newTitle" + currentNoteSections.length});
-
+    }
     return html;
 }
 
 /**
  * This function returns the html for a new simple text section
  */
-function addSimpleTextSection(){
-    let html= `<div class="section">
+function addSimpleTextSection(mode, value, newSection, internalSectionId){
+    let html;
+
+    if(mode === 0){
+        html = `<div class="section">
+                    <p class="section-display-simpletext">${value}</p>
+                </div>`
+    }else {
+        html = `<div class="section">
                         <div class="new-title-container">
+                            ${getActionHtml()}
                             <label class="label">Simple Text</label>
-                            <textarea class="input-textarea" id="newSimpleText${currentNoteSections.length+1}" rows="7"/>
+                            <textarea class="input-textarea" id="newSimpleText${internalSectionId}" rows="7">${value}</textarea>
                         </div>
                       </div>`;
 
-    currentNoteSections.push({type: 2, editor: null, fieldRef: "#newSimpleText" + currentNoteSections.length});
+        if (newSection) {
+            currentNoteSections.push({
+                type: 2,
+                editor: null,
+                fieldRef: "#newSimpleText" + internalSectionId,
+                order: internalSectionId
+            });
+        }else{
+            currentNoteSections[internalSectionId-1].fieldRef = "#newSimpleText" + internalSectionId;
+        }
+    }
 
     return html;
 }
@@ -186,13 +381,19 @@ function addSimpleTextSection(){
  * This function returns the html for a new text section
  * @returns {*}
  */
-function addRichTextSection(){
-    let html= `<div class="section">
-                <div id="editor${currentNoteSections.length+1}">
+function addRichTextSection(mode, value, newSection, internalSectionId){
+    let html;
+
+    if (mode === 0){
+
+    }else{
+        html= `<div class="section">
+                <div id="editor${internalSectionId}">
                 </div>
             </div>`;
 
-    currentNoteSections.push({type: 3, editor: null, fieldRef: null});
+        currentNoteSections.push({type: 3, editor: null, fieldRef: null});
+    }
 
     return html;
 }
@@ -202,4 +403,143 @@ function addRichTextSection(){
  */
 function addVideoSection(){
 
+}
+
+/**
+ * This function upserts a training note with all its sections
+ */
+function upsertTrainingNote() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/trainingNotes/upsert',
+            type: 'POST',
+            data: { note: currentNote, sections: currentNoteSections },
+            success: function(data) {
+                loadExistingNotes().then((result) => {
+                    allNotes = result;
+                    displaySuccess(data.message);
+                    resolve();
+                }).fail((error) => {
+                    console.error("Error refreshing allNotes data after upsert:", error);
+                    reject(error);
+                });
+            },
+            error: function(data) {
+                if (data.responseJSON && data.responseJSON.redirect) {
+                    window.location.href = data.responseJSON.redirect;
+                }
+                console.log("Error upserting training notes:", data.responseJSON);
+                reject(data); // Reject the promise with the error data
+            }
+        });
+    });
+}
+
+/**
+ * Returns the html for all buttons in edit mode
+ */
+function getActionHtml(){
+    return `<div class="action-container">
+                <a href="" class="edit-icon"><i class="ri-arrow-up-line ri-xl"></i></a>
+                <a href="" class="edit-icon"><i class="ri-arrow-down-line ri-xl"></i></a>
+                <a href="" class="edit-icon"><i class="ri-close-circle-line ri-xl"></i></a>
+            </div>`
+}
+
+/**
+ * Function to move a section up
+ * @param index
+ */
+function moveSectionUp(index) {
+    if (index > 0) {
+        const tmp = currentNoteSections[index];
+        currentNoteSections[index] = currentNoteSections[index - 1];
+        currentNoteSections[index - 1] = tmp;
+    }
+}
+
+/**
+ * Function to move a section down
+ * @param index
+ */
+function moveSectionDown(index) {
+    if (index < currentNoteSections.length - 1) {
+        const tmp = currentNoteSections[index];
+        currentNoteSections[index] = currentNoteSections[index + 1];
+        currentNoteSections[index + 1] = tmp;
+    }
+}
+
+/**
+ * Function to delete a section
+ * @param index
+ */
+function deleteSection(index) {
+    let section = currentNoteSections[index];
+    currentNoteSections.splice(index, 1);
+
+    if (section.id){
+        //Delete the section in the database
+        $.ajax({
+            url: '/trainingNotes/deleteSection',
+            type: 'POST',
+            data: { sectionId: section.id },
+            error: function(data) {
+                if (data.responseJSON && data.responseJSON.redirect) {
+                    window.location.href = data.responseJSON.redirect;
+                }
+                console.log("Error deleting section: ", data.responseJSON);
+            }
+        });
+    }
+
+    editNote();
+}
+
+/**
+ * This function deletes the current note
+ */
+function deleteNote(){
+    $.ajax({
+        url: '/trainingNotes/delete',
+        type: 'POST',
+        data: { noteId: currentNote.id },
+        success: function(data) {
+            displaySuccess(data.message);
+            currentNote = null;
+            currentNoteSections = [];
+            currentSection = null;
+
+            $("#editNoteContainer").hide();
+            $("#editBtnContainer").hide();
+            $("#nothingPlaceholder").show();
+            buildExistingNodesTable();
+        },
+        error: function(data) {
+            if (data.responseJSON && data.responseJSON.redirect) {
+                window.location.href = data.responseJSON.redirect;
+            }
+            console.log("Error deleting note: ", data.responseJSON);
+        }
+    });
+}
+
+/**
+ * This function creates a new note
+ */
+function createNewNote(){
+    if ($("#editNoteContainer").is(":visible")) {
+        displayError("Please save or cancel your changes first!")
+        return;
+    }
+
+    $("#editNoteContainer").show();
+    $("#editBtnContainer").show();
+    $("#nothingPlaceholder").hide();
+    $("#noteDisplayContainer").hide();
+    $("#editNote").hide();
+
+    $("#editTitle").val("");
+
+    currentNote = {};
 }
--- routes/trainingNotesRouter.js
+++ routes/trainingNotesRouter.js
@@ -20,14 +20,167 @@ router.get('/getAll', checkNotAuthenticated, permissionCheck('trainingnotes', 'c
 });
 
 /**
- *
+ * GET route for the sections of a training note
+ */
+router.get('/getSections', checkNotAuthenticated, permissionCheck('trainingnotes', 'canOpen'), function (req, res) {
+    getSections(req.query.noteId).then((result) => {
+        res.status(200).send(result.rows);
+    }).catch((err) => {
+        console.log("Error: " + err);
+        res.status(500).send({message: "There was an error getting the Sections! Please try again later."});
+    });
+});
+
+/**
+ * POST route for deleting a section
+ */
+router.post('/deleteSection', checkNotAuthenticated, permissionCheck('trainingnotes', 'canOpen'), function (req, res) {
+    deleteSection(req.body.sectionId).then((result) => {
+        res.status(200).send({message: "Successfully deleted the section!"});
+    }).catch((err) => {
+        console.log("Error: " + err);
+        res.status(500).send({message: "There was an error deleting the Sections! Please try again later."});
+    });
+});
+
+/**
+ * POST route for deleting a note
+ */
+router.post('/delete', checkNotAuthenticated, permissionCheck('trainingnotes', 'canOpen'), function (req, res) {
+    deleteNote(req.body.noteId).then((result) => {
+        res.status(200).send({message: "Successfully deleted the note!"});
+    }).catch((err) => {
+        console.log("Error: " + err);
+        res.status(500).send({message: "There was an error deleting the note! Please try again later."});
+    });
+});
+
+/**
+ * POST route for upserting sections of a Training Note
+ */
+router.post('/upsert', checkNotAuthenticated, permissionCheck('trainingnotes', 'canOpen'), function (req, res) {
+    upsertTrainingNote(req.body.note.title, req.user.id, req.body.note.id, req.user.team.id).then((returnedNoteId) => {
+        upsertSections(req.body.sections, returnedNoteId) // Pass the returnedNoteId here
+            .then(() => {
+                res.status(200).send({message: "Successfully updated the training note!"});
+            })
+            .catch((err) => {
+                console.log("Error: " + err);
+                res.status(500).send({message: "There was an error updating the sections of the note! Please try again later."});
+            });
+    })
+        .catch((err) => {
+            console.log("Error: " + err);
+            res.status(500).send({message: "There was an error upserting the training note! Please try again later."});
+        });
+});
+
+/**
+ * This function returns all training notes for a team
  */
 function getTrainingNotes(teamId){
     return pool.query(`SELECT trainingnotes.id, acc1.username AS creator, acc2.username AS editor, TO_CHAR(created, 'DD.MM.YYYY HH24:MI') AS created, 
-                                            TO_CHAR(lastedited, 'DD.MM.YYYY HH24:MI') AS lastedited, title, notes FROM trainingnotes
+                                            TO_CHAR(lastedited, 'DD.MM.YYYY HH24:MI') AS lastedited, title FROM trainingnotes
                                         LEFT JOIN account AS acc1 ON acc1.id = creator
                                         LEFT JOIN account AS acc2 ON acc2.id = editor
-                                        WHERE team_fk = $1`, [teamId]);
+                                        WHERE team_fk = $1 ORDER BY lastedited DESC`, [teamId]);
+}
+
+/**
+ * This function upserts all sections for a training note
+ */
+function upsertSections(sections = [], noteId) {
+    if (!sections.length) return Promise.resolve();
+
+    const promises = sections.map((section) => {
+        return new Promise((resolve, reject) => {
+            if (section.id) {
+                // Update section
+                pool.query(`UPDATE section SET value = $1, "order" = $3 WHERE id = $2`, [section.value, section.id, section.order], (err, results) => {
+                    if (err) {
+                        console.log("Error updating section: " + err);
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            } else {
+                // Insert section
+                pool.query(`INSERT INTO section (trainingnotes_fk, type, value, "order") VALUES ($1, $2, $3, $4)`, [noteId, section.type, section.value, section.order], (err, results) => {
+                    if (err) {
+                        console.log("Error inserting section: " + err);
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            }
+        });
+    });
+
+    return Promise.all(promises);
+}
+
+/**
+ * This function returns all sections for a training note
+ */
+function getSections(noteId){
+    return pool.query(`SELECT * FROM section WHERE trainingnotes_fk = $1 ORDER BY "order" ASC`, [noteId]);
+}
+
+/**
+ * This function deletes a section
+ * @param sectionId
+ * @returns {Promise<QueryResult<any>>}
+ */
+function deleteSection(sectionId){
+    return pool.query(`DELETE FROM section WHERE id = $1`, [sectionId]);
+}
+
+/**
+ * This function updates a training note
+ * @param title
+ * @param editorId
+ * @param noteId
+ * @param teamId
+ * @returns {Promise<QueryResult<any>>}
+ */
+function upsertTrainingNote(title, editorId, noteId, teamId){
+    return new Promise((resolve, reject) => {
+        if (noteId > 0) {
+            // Update
+            pool.query(`UPDATE trainingnotes SET title = $1, editor = $2, lastedited = NOW() WHERE id = $3 RETURNING id`, [title, editorId, noteId], (err, results) => {
+                if (err) {
+                    console.log("Error updating training note: " + err);
+                    reject(err);
+                } else {
+                    resolve(results.rows[0].id); // Return the noteId
+                }
+            });
+        } else {
+            // Insert
+            pool.query(`INSERT INTO trainingnotes (title, editor, lastedited, creator, created, team_fk) VALUES ($1, $2, NOW(), $2, NOW(), $3) RETURNING id`, [title, editorId, teamId], (err, results) => {
+                if (err) {
+                    console.log("Error inserting training note: " + err);
+                    reject(err);
+                } else {
+                    resolve(results.rows[0].id); // Return the newly inserted noteId
+                }
+            });
+        }
+    });
+}
+
+/**
+ * This function deletes a training note
+ * It cascades to the sections
+ * @param noteId
+ */
+function deleteNote(noteId){
+    return pool.query(`DELETE FROM section WHERE trainingnotes_fk = $1`, [noteId])
+        .then(() => {
+            return pool.query(`DELETE FROM trainingnotes WHERE id = $1`, [noteId]);
+        });
 }
 
 module.exports = router;
